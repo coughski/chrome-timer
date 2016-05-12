8e39eb15227954dbede2278aece3041bd213f0cf
Andrei Cioara <andrei@cioara.me>
2016-05-12 20:52:52 +0100

    [Refactor] Every file finishes with a new line. No more trailing spaces

--- popup.js
+++ popup.js
@@ -34,26 +34,26 @@ function load()
     hide("modify");
     hide("resume");
     editing = false;
-    
+
     // if timer is paused, show resume button and hide pause button
     if(bgpage.pauseDate)
     {
         showInline("resume");
         hide("pause");
     }
-   
+
 	// loads custom times if they exist
 	for(var i = 0; i < document.choices.radio.length; i++)
 		if(localStorage[i] != null)
 			document.getElementById("s"+i).textContent = localStorage[i];
-    
+
    // if timer is off, show settings
 	if(!bgpage.alarmDate)
 	{
 		show("settings");
       hide("display");
 	}
-	
+
 	// else, show countdown
 	else
 	{
@@ -78,23 +78,23 @@ function getChoice()
 function swap()
 {
 	editing = true;
-	
+
 	// swap text with fields
 	for(var i = 0; i < document.choices.radio.length; i++)
 	{
 		var span = document.getElementById("s"+i);
 		var num = parseInt(span.textContent);
-		
+
 		previousValues[i] = num;
-		
+
 		var html = "<input class='input-mini' type='text' name='custom' id='c"+i;
 		html += "' value='"+num;
 		html += "'>";
 		// used to select on click and auto save on change
-        
+
 		span.innerHTML = html;
 	}
-    
+
 	// swap edit button with done button
 	var butt = document.getElementById("swapper");
 	butt.innerHTML = "<a href='#' id='done' class='btn'><i class='icon-ok'></i></a>";
@@ -108,7 +108,7 @@ function swapBack()
 	{
 		var span = document.getElementById("s"+i);
 		var num = parseInt(document.getElementById("c"+i).value);
-		
+
 		if(isValid(num))
         {
             localStorage[i] = num;
@@ -117,12 +117,12 @@ function swapBack()
 		else
 			span.textContent = previousValues[i];
 	}
-	
+
 	// swap done button with edit button
 	var butt = document.getElementById("swapper");
 	butt.innerHTML = "<a href='#' id='wrench' class='btn'><i class='icon-wrench'></i></a>";
    document.querySelector('#wrench').addEventListener('click', swap);
-	
+
 	editing = false;
 }
 
@@ -131,12 +131,12 @@ function setTimer()
 	// make sure we're dealing with text not fields
 	if(editing)
 		swapBack();
-	
+
 	// SET background timer for selected number
 	// HIDE settings, DISPLAY countdown
-    
+
 	var num = getChoice();
-	
+
 	// set timer, hide settings, display reset button
 	if(isValid(num))
 	{
@@ -164,12 +164,12 @@ function isValid(amt)
 function refreshDisplay()
 {
    percent = bgpage.getTimeLeftPercent();
-   
+
    if(percent < 15)
       document.getElementById("bar").style.color = "grey";
 	document.getElementById("bar").textContent = bgpage.getTimeLeftString();
    document.getElementById("bar").style.width = percent + "%";
-    
+
 	refreshDisplayTimeout = setTimeout(refreshDisplay, 100);
 }
 
